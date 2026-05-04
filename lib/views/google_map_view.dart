8bfddbf16394843021b7f27ce4ff4dc22a86d29e
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-05-04 17:01:34 +0300

    create google map view

---
+++ lib/views/google_map_view.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class GoogleMapView extends StatelessWidget {
+  const GoogleMapView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
