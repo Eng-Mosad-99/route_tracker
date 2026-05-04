2df9c73363099e57c737731bb51b2d1a721324fd
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-05-04 17:05:02 +0300

    add google map widget  to show  map

--- lib/views/google_map_view.dart
+++ lib/views/google_map_view.dart
@@ -1,13 +1,29 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class GoogleMapView extends StatelessWidget {
+class GoogleMapView extends StatefulWidget {
   const GoogleMapView({super.key});
+
+  @override
+  State<GoogleMapView> createState() => _GoogleMapViewState();
+}
+
+class _GoogleMapViewState extends State<GoogleMapView> {
+  late CameraPosition initialCameraPosition;
+
+  @override
+  void initState() {
+    initialCameraPosition = CameraPosition(target: LatLng(0, 0));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: GoogleMap(
+        initialCameraPosition: initialCameraPosition,
+        zoomControlsEnabled: false,
+      ),
     );
   }
 }
